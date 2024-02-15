fcbee39e2e5741add8c207a2b8d56989643a19a8
mekyugorji7 <131839388+mekyugorji7@users.noreply.github.com>
2024-02-15 10:51:28 -0500

    Update mips.s
    Start mips code by calling main and displaying the input prompt to the user

--- mips.s
+++ mips.s
@@ -5,3 +5,17 @@
    
 
 # Determine base_n, beta, and delta values based on your Howard ID
+# Howard ID : 03057357
+# N = 26 + (3057357 % 11) = 26 + 6 = 32
+# M = 22
+# Beta: 'v'
+# Delta: 'V'
+
+.text
+.globl main
+
+main:
+    # Display prompt
+    li $v0, 4
+    la $a0, prompt
+    syscall
